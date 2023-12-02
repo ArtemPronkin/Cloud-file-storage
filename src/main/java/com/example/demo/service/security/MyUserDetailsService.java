4094f7e87be6d35df71c4f337af6853153605d4a
Artyom <tema.ru2011@gmail.com>
2023-12-02 17:28:35 +0300

    Refactor: перенес MyPrincipal и MyUserDetailsService в папку security, удалил валидатор

--- src/main/java/com/example/demo/service/security/MyUserDetailsService.java
+++ src/main/java/com/example/demo/service/security/MyUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.service.security;
 
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
